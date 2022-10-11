9546cd00011d606b33594aa2ff94cd19da0a7ee7
Meck <76256502+Karibura-Cyber@users.noreply.github.com>
2022-10-11 19:31:04 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ page = requests.get(URL)
 data = page.json()
 now = []
 old = []
-
-with open('atc.json', 'w') as f:
-    json.dump([], f)
+atc_list = []
 
 @bot.event
 async def on_ready():
@@ -36,28 +34,21 @@ async def on_ready():
         for i in old: #get all callsign from old list
             if i not in now: #if the callsign is not in list now
                 print("[\033[91mOFFLINE\033[0m] {} [{}]".format(i, time.strftime("%H:%M:%S"))) 
-                channel = bot.get_channel(id=int(config['channel'])) 
                 embed=discord.Embed()
                 embed=discord.Embed(title="{} is now offline".format(i), color=0xff0000)
                 embed.set_thumbnail(url="https://pbs.twimg.com/profile_images/1391075311174000649/8--HPB9-_400x400.jpg")
-                channel = bot.get_channel(id=int(config['channel'])) #channel
-                msg = await channel.send(embed=embed) #send message to channel
-                #delete message after 10 seconds
-                await msg.delete(delay=10)
-                data = json.load(open('atc.json')) #read json file
+                msg = await bot.get_channel(int(config['channel'])).send(embed=embed) #Send Offline status when ATC is Offline
+                await msg.delete()
+                data = atc_list #json.load(open('atc.json')) #read json file
 
                 for x in data: #if the callsign is in json file
                     if x['callsign'] == i:
                         print("[\u001b[32mSUCCESS\u001b[37m] Delete MSG ID: {}".format(x['msg_id']))
-                        del_msg = await channel.fetch_message(x['msg_id']) #get message id
+                        del_msg = await bot.get_channel(int(config['channel'])).fetch_message(x['msg_id']) #get message id
                         await del_msg.delete() #delete message
                         data.remove(x) #remove the callsign from json file
                         with open('atc.json', 'w') as f:
-                            json.dump(data, f)
-                else:
-                    print("[\033[91mERROR\033[0m] {} not found in json file".format(i))
-
-                
+                            json.dump(data, f)              
 
 
         for a in now: #get data from now list                  
@@ -65,7 +56,6 @@ async def on_ready():
                 atc = data['clients']['atcs']
                 for i in atc:     
                     if i['callsign'] == a:
-                        serverID = i['serverId']
                         frequency = i['atcSession']['frequency']
                         createdAt = i['createdAt']
                         atis = i['atis']
@@ -88,24 +78,11 @@ async def on_ready():
                         embed.add_field(name="Frequency", value="{}MHz".format(frequency), inline=False) #Freq
                         embed.add_field(name="ATIS", value='\n'.join(map(str, lines)), inline=False) #ATIS of ATC
                         embed.add_field(name="createdAt", value=f"{createdAt}", inline=False)
-                        channel = bot.get_channel(id=int(config['channel'])) #channel
-                        message = await channel.send(embed=embed) #send message to channel is online
-                        
+                        message = await bot.get_channel(int(config['channel'])).send(embed=embed) #Send Embed when ATC is online
                         #append message id to json file
-                        with open('atc.json') as fp:
-                            listObj = json.load(fp)
-                            listObj.append({"callsign": a, "msg_id": message.id})
-                        with open('atc.json', 'w') as json_file:
-                            json.dump(listObj, json_file, 
-                                                indent=4,  
-                                                separators=(',',': '))
+                        atc_list.append({"callsign": a, "msg_id": message.id})
 
-
-
-
-
-
-                        
+                     
         for old_atc in content['clients']['atcs']: #find all ATC from previous JSON file
             old_callsign = old_atc['callsign'] #find callsign from old JSON file
             if config['callsign_prefix'] in old_callsign[0:2]: #check if ATC is your country
