b8e202e1b2a6db72da1551c22085bcd619244fe5
Meck <76256502+Karibura-Cyber@users.noreply.github.com>
2023-05-02 16:55:23 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -19,16 +19,17 @@ data = page.json()
 now = []
 old = []
 atc_list = []
-current_version = 1
+pilot_list = []
+current_version = 1.2
 
 @bot.event
 async def on_ready():
-    print(f"[\033[33mINFO\033[0m] {bot.user.name} is now online. current version is {current_version}") #Prints the bot name and status
+    print(f"[\u001b[32mSUCCESS\u001b[37m] {bot.user.name} is now online. current version is {current_version}") #Prints the bot name and status
     print("[\033[33mINFO\033[0m] Send Message to channel.", config['channel']) #Prints the channel name
     online_check.start()
-    print("[\033[33mINFO\033[0m] Online Check started!")
+    print("[\u001b[32mSUCCESS\u001b[37m] Online Check started!")
     version_check.start()
-    print("[\033[33mINFO\033[0m] Version Check started!")
+    print("[\u001b[32mSUCCESS\u001b[37m] Version Check started!")
 
 @tasks.loop(seconds=30)
 async def online_check():
@@ -92,6 +93,7 @@ async def online_check():
                         message = await bot.get_channel(int(config['channel'])).send(embed=embed) #Send Embed when ATC is online
                         #append message id to json file
                         atc_list.append({"callsign": a, "msg_id": message.id})
+                    
             except:
                 pass
         else:
@@ -108,6 +110,47 @@ async def online_check():
         old.append(i) #append data from now list to old list
     now.clear() #clear all data in now list
 
+    # Check if flights are online
+    for pilot in data['clients']['pilots']:
+        pilot_callsign = pilot['callsign']
+        if pilot_callsign not in pilot_list:
+            if pilot_callsign[0:3] in config['flight_callsign_prefix']:
+                # Extract necessary information
+                user_id = pilot['userId']
+                lat = pilot['lastTrack']['latitude']
+                lon = pilot['lastTrack']['longitude']
+                transponder = pilot['lastTrack']['transponder']
+                aircraft_id = pilot['flightPlan']['aircraftId']
+                departure_id = pilot['flightPlan']['departureId']
+                arrival_id = pilot['flightPlan']['arrivalId']
+                
+                # Create an embed to display information
+                embed = discord.Embed(title=f"Callsign: {pilot_callsign}", color=0x00ff00)
+                embed.set_thumbnail(url="https://pbs.twimg.com/profile_images/1391075311174000649/8--HPB9-_400x400.jpg")
+                embed.add_field(name="UserID", value=user_id, inline=False)
+                embed.add_field(name="Lat", value=lat, inline=False)
+                embed.add_field(name="Lon", value=lon, inline=False)
+                embed.add_field(name="Transponder", value=transponder, inline=False)
+                embed.add_field(name="Aircraft ID", value=aircraft_id, inline=False)
+                embed.add_field(name="Departure", value=departure_id, inline=False)
+                embed.add_field(name="Arrival", value=arrival_id, inline=False)        
+                embed.add_field(name="Click to view", value=f"https://webeye.ivao.aero/?pilotId={pilot['id']}", inline=False)
+                
+                # Send the embed and add the callsign to the list of pilots
+                message = await bot.get_channel(int(config['channel'])).send(embed=embed)
+                print(f"[ONLINE] {pilot_callsign} [{time.strftime('%H:%M:%S')}]")
+                print(f"[SUCCESS] {pilot_callsign} data loaded!")
+                pilot_list.append(pilot_callsign)
+
+        else:
+            # Check if callsign is still in data, remove it from the list if it's not and send an embed to indicate that it's offline
+            if pilot_callsign not in [pilot['callsign'] for pilot in data['clients']['pilots']]:
+                pilot_list.remove(pilot_callsign)
+                embed = discord.Embed(title=f"Callsign: {pilot_callsign} is offline", color=0x00ff00)
+                message = await bot.get_channel(int(config['channel'])).send(embed=embed)
+
+
+
 @tasks.loop(seconds=10)
 async def version_check():
     r = requests.get("https://raw.githubusercontent.com/Karibura-Cyber/IVAO-ATC-Status/main/version.txt")
@@ -116,5 +159,32 @@ async def version_check():
         print(f"[\033[91mWARNING\033[0m] IVAO ATC Status {web_version} version released! Please update at https://github.com/Karibura-Cyber/IVAO-ATC-Status") 
         await bot.get_channel(int(config['channel'])).send(f"IVAO ATC Status {web_version} version released! Please update")
         version_check.stop()
+
+#Command sections
+@bot.command()
+async def track(ctx, flight_number: str):     
+     for i in data['clients']['pilots']:
+        if i['callsign'] == flight_number.upper():
+            #make embed show callsign, userid, lastTrack latitude and longitude, transponder, flightPlan aircraftId departureId and arrivalId
+            userID = i['userId']
+            lat = i['lastTrack']['latitude']
+            lon = i['lastTrack']['longitude']
+            transponder = i['lastTrack']['transponder']
+            aircraft_id = i['flightPlan']['aircraftId']
+            departure_id = i['flightPlan']['departureId']
+            arrival_id = i['flightPlan']['arrivalId']
+
+            embed=discord.Embed()
+            embed=discord.Embed(title="Callsign: {}".format(flight_number), color=0x00ff00)
+            embed.set_thumbnail(url="https://pbs.twimg.com/profile_images/1391075311174000649/8--HPB9-_400x400.jpg")
+            embed.add_field(name="UserID", value="{}".format(userID), inline=False)
+            embed.add_field(name="Lat", value="{}".format(lat), inline=False)
+            embed.add_field(name="Lon", value="{}".format(lon), inline=False)
+            embed.add_field(name="Transponder", value="{}".format(transponder), inline=False)
+            embed.add_field(name="Aircraft ID", value="{}".format(aircraft_id), inline=False)
+            embed.add_field(name="Departure", value="{}".format(departure_id), inline=False)
+            embed.add_field(name="Arrival", value="{}".format(arrival_id), inline=False)        
+            embed.add_field(name="Click to view", value="https://webeye.ivao.aero/?pilotId={}".format(i['id']), inline=False)
+            message = await ctx.send(embed=embed)
 
 bot.run(TOKEN)
